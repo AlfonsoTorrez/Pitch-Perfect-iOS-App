9570eaab44d0109becb619e565c0e2dafb94698b
Alfonso Torres <alftaurus@Alfonsos-MacBook-Pro.local>
2017-10-24 16:17:04 -0700

    Added GSignInButton

--- GSignInDemo/GSignInDemo/ViewController.swift
+++ GSignInDemo/GSignInDemo/ViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import Google
+import GoogleSignIn
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
+    //Label will be used to see the users email
+    @IBOutlet weak var emailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //error object
+        var error : NSError?
+        
+        //setting the error
+        GGLContext.sharedInstance().configureWithError(&error)
+        
+        //if any error stop execution and print error
+        if error != nil{
+            print(error ?? "google error")
+            return
+        }
+        
+        //adding the delegates
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
+        
+        //getting the signin button and adding it to view
+        let googleSignInButton = GIDSignInButton()
+        googleSignInButton.center = view.center
+        view.addSubview(googleSignInButton)
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+        //if any error stop and print the error
+        if error != nil{
+            print(error ?? "google error")
+            return
+        }
+        
+        //if success display the email on label
+        emailLabel.text = user.profile.email
     }
 
     override func didReceiveMemoryWarning() {
