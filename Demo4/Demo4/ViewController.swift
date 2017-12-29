4068a0021e8f3c1a5af8d6f0b9ab7c73ee70aa24
AlfonsoTorrez <alftorres@csumb.edu>
2017-12-29 15:39:51 -0800

    Demo4

---
+++ Demo4/Demo4/ViewController.swift
@@ -0,0 +1,137 @@
+//
+//  ViewController.swift
+//  Demo4
+//
+//  Created by Alfonso Torres on 12/28/17.
+//  Copyright © 2017 Alfonso Torres. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    //var names: [String] = []
+    var notes: [NSManagedObject] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "The List"
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "Cell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        //2
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Note")
+        
+        //3
+        do {
+            notes = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    //Action from the add button in the nav bar
+    @IBAction func addNote(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "New Note",
+                                      message: "Add a new note",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) {
+                                        [unowned self] action in
+                                        
+                                        guard let textField = alert.textFields?.first,
+                                            let nameToSave = textField.text else {
+                                                return
+                                        }
+                                        
+                                        self.save(note: nameToSave)
+                                        self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .default)
+        
+        alert.addTextField()
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+    
+    func save(note: String) {
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Note",
+                                       in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        
+        // 3
+        person.setValue(note, forKeyPath: "info")
+        
+        // 4
+        do {
+            try managedContext.save()
+            notes.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+
+    
+
+}
+
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell {
+            
+            let person = notes[indexPath.row]
+            let cell =
+                tableView.dequeueReusableCell(withIdentifier: "Cell",
+                                              for: indexPath)
+            cell.textLabel?.text =
+                person.value(forKeyPath: "info") as? String
+            return cell
+    }
+}
+
